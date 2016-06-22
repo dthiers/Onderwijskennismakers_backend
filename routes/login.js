31ff46871d4e70b728b048cc23b17b273d3032ce
Tijn Kanters <trkanter@avans.nl>
2016-06-22 17:43:24 +0200

    jwt added to the api

--- routes/login.js
+++ routes/login.js
@@ -10,7 +10,7 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function(passport){
-
+    var jwt = require('jwt-simple');
   /**
   *
   * Route for LocalStategy login
@@ -19,9 +19,19 @@ module.exports = function(passport){
   router.route('/local')
     // '/local/login' POST to login
     .post(passport.authenticate('local'), function(req, res, next) {
+       
+        // TODO: hier een JWT token aanmaken en die terug sturen naar de user
+        var token = jwt.encode({
+            id: req.user.id,
+            username: req.user.firstName
+        }, 'secret');
+        
+        console.log(req.user);
+       
       //res.return(req.user, 'data', 'je moeder');
       res.json({
         data: req.user,
+        token: token,
         success: true,
         message: 'Success dr mee'
       })
